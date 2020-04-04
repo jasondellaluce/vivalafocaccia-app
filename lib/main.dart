79e73c05d151a15e77b6eaad319f13bfcdecf3de
jasondellaluce <jdellalu@uci.edu>
2020-04-03 22:00:08 -0700

    Initial search page UI implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,76 @@
 import 'package:app/model/repositories.dart';
-import 'package:app/pages/recipe_search_page.dart';
 import 'package:app/bloc/recipe_search_bloc.dart';
+import 'package:app/pages/recipe_search_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 
-void main() {
-  runApp(App(
-      recipeRepository: ModelRepositoryFactory.instance.getRecipeRepository()
-  ));
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+    runApp(VivaLaFocacciaApp(recipeRepository: ModelRepositoryFactory.instance.getRecipeRepository()));
+  });
 }
 
-class App extends StatelessWidget {
+class VivaLaFocacciaApp extends StatefulWidget {
+
+  static Color lightPrimary = Color(0xfffcfcff);
+  static Color darkPrimary = Colors.black;
+  static Color lightAccent = Color(0xff5563ff);
+  static Color darkAccent = Color(0xff5563ff);
+  static Color lightBG = Color(0xfffcfcff);
+  static Color darkBG = Colors.black;
+  static Color ratingBG = Colors.yellow[600];
+  static ThemeData lightTheme = ThemeData(
+      backgroundColor: lightBG,
+      primaryColor: lightPrimary,
+      accentColor:  lightAccent,
+      cursorColor: lightAccent,
+      scaffoldBackgroundColor: lightBG,
+      appBarTheme: AppBarTheme(
+        textTheme: TextTheme(
+          headline6: TextStyle(
+            color: darkBG,
+            fontSize: 18.0,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+      )
+  );
+
   final RecipeRepository recipeRepository;
 
-  const App({
+  const VivaLaFocacciaApp({
     Key key,
     @required this.recipeRepository,
   }) : super(key: key);
 
   @override
+  _VivaLaFocacciaAppState createState() => _VivaLaFocacciaAppState();
+}
+
+class _VivaLaFocacciaAppState extends State<VivaLaFocacciaApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: VivaLaFocacciaApp.lightPrimary,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'VivaLaFocaccia',
+      debugShowCheckedModeBanner: false,
+      theme: VivaLaFocacciaApp.lightTheme,
+      title: "VivaLaFocaccia",
       home: BlocProvider(
         create: (context) =>
-            RecipeSearchBloc(recipeRepository: recipeRepository),
+            RecipeSearchBloc(recipeRepository: widget.recipeRepository),
         child: RecipeSearchPage(),
       ),
     );
